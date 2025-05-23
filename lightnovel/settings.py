553af0c73e73c8ba73e79f697219e5f61bd682ef
huydt747 <103411739+huydt747@users.noreply.github.com>
2025-05-23 21:02:06 +0700

    Update settings.py

--- lightnovel/settings.py
+++ lightnovel/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8e^^rju5qm3h9^lfx)iiz(z_rysc$y%+)x)5j0wz@173hz)6ow'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ffffffa111.pythonanywhere.com', '127.0.0.1']
 
